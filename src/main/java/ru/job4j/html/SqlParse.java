be590866b614feab204a5442050e9a0e55c00c07
KarnaukhovKirill <kirya2361@gmail.com>
2021-08-11 12:22:19 +0300

    Del later

--- src/main/java/ru/job4j/html/SqlParse.java
+++ src/main/java/ru/job4j/html/SqlParse.java
@@ -4,19 +4,17 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import ru.job4j.grabber.Post;
 import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Writer;
 import java.time.format.DateTimeFormatter;
 
 public class SqlParse {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd MMMM yyyy, HH:mm");
     public static void main(String[] args) throws IOException {
+        SqlRuDateTimeParser parser = new SqlRuDateTimeParser();
         for (int j = 1; j <= 5; j++) {
-            SqlRuDateTimeParser parser = new SqlRuDateTimeParser();
+            //SqlRuDateTimeParser parser = new SqlRuDateTimeParser();
             var page = String.format("https://www.sql.ru/forum/job-offers/%d", j);
             Document doc = Jsoup.connect(page).get();
             Elements row = doc.select(".postslisttopic");
@@ -24,7 +22,7 @@ public class SqlParse {
             for (int i = 3; i < row.size(); i++) {
                 Element td = row.get(i);
                 Element href = td.child(0);
-                //Element date = rowDate.get(i);
+                Element date = rowDate.get(i);
                 parseJobPage(href);
                 //System.out.println(href.attr("href"));
                 //System.out.println(href.text());
@@ -40,9 +38,10 @@ public class SqlParse {
         Document doc = Jsoup.connect(href.attr("href")).get();
         var msgBody = doc.select(".msgBody").get(1);
         var description = msgBody.text();
-        System.out.println(description);
+        //System.out.println(description);
         var msgFooter = doc.select("td.msgFooter").get(0);
         var time = msgFooter.text().split(" \\[")[0];
-        System.out.println(time);
+        var title = doc.select("td.messageHeader").text();
+        System.out.println(title);
     }
 }
