41da16c8b92a2fd86ef0d132f5c0ed1f7bfa9d35
KarnaukhovKirill <kirya2361@gmail.com>
2021-08-13 16:47:17 +0300

    web

--- src/main/java/ru/job4j/grabber/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/grabber/Grabber.java
@@ -2,12 +2,17 @@ package ru.job4j.grabber.grabber;
 
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
+import ru.job4j.grabber.models.Post;
 import ru.job4j.grabber.parse.Parse;
 import ru.job4j.grabber.parse.SqlRuParse;
 import ru.job4j.grabber.store.PsqlStore;
 import ru.job4j.grabber.store.Store;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.newJob;
@@ -64,11 +69,35 @@ public class Grabber implements Grab {
         }
     }
 
+    public void web(Store store) {
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(Integer.parseInt(cfg.getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store.getAll()) {
+
+                            out.write(
+                                    post.toString().getBytes((Charset.forName("Windows-1251"))));
+                            out.write(System.lineSeparator().getBytes());
+                        }
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
     public static void main(String[] args) throws Exception {
         Grabber grab = new Grabber();
         grab.cfg();
         Scheduler scheduler = grab.scheduler();
         Store store = grab.store();
         grab.init(new SqlRuParse(), store, scheduler);
+        grab.web(store);
     }
 }
